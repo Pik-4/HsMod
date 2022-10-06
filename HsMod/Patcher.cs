1837dce7af962c5590c1d81b4d87042aa2285cc8
pik-4 <88079004+Pik-4@users.noreply.github.com>
2022-10-07 03:33:10 +0800

    Revert "1.3.12.3"
    This reverts commit d2da84e23ad4fdbc166b0ea1be1c4867f363bf98.

--- HsMod/Patcher.cs
+++ HsMod/Patcher.cs
@@ -1804,20 +1804,11 @@ namespace HsMod
                 //string rawCardId = cardId;
                 else if (cardId != null && DefLoader.Get()?.GetEntityDef(cardId)?.GetCardType() == TAG_CARDTYPE.HERO_POWER)
                 {
-                    if (isSkinDefalutHeroEnable.Value)
+                    if (skinHero.Value == 637 && skinOpposingHero.Value == 637)    // 防止技能识别错误，待测试、待验证
                     {
-                        try
-                        {
-                            TAG_CLASS tagClass = DefLoader.Get().GetEntityDef(cardId).GetClass();
-                            cardId = GameUtils.GetHeroPowerCardIdFromHero(Enumerable.FirstOrDefault(Enumerable.Where(GameDbf.CardHero.GetRecords().OrderBy(x => x.CardId).ToList(), (CardHeroDbfRecord x) => DefLoader.Get().GetEntityDef(x.CardId).GetClass() == tagClass)).CardId);
-                            return;
-                        }
-                        catch (Exception ex)
-                        {
-                            Utils.MyLogger(BepInEx.Logging.LogLevel.Error, ex);
-                        }
+                        return;
                     }
-                    if (skinHero.Value != -1 && __instance.GetCard().GetControllerSide() == global::Player.Side.FRIENDLY)
+                    else if (skinHero.Value != -1 && __instance.GetCard().GetControllerSide() == global::Player.Side.FRIENDLY)
                     {
                         cardId = GameUtils.GetHeroPowerCardIdFromHero(skinHero.Value);
                     }
