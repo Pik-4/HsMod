4fdceecfb06f8fed63a74fbc083b163f5a421b4a
Pik-4 <88079004+Pik-4@users.noreply.github.com>
2024-01-20 08:34:06 +0800

    5.1.0.0

--- HsMod/PluginInfo.cs
+++ HsMod/PluginInfo.cs
@@ -5,6 +5,6 @@
         public const string PLUGIN_GUID = "HsMod";
         public const string PLUGIN_NAME = "HsMod";
         public const string PLUGIN_AUTHOR = "Pik_4";
-        public const string PLUGIN_VERSION = "5.0.1.2";
+        public const string PLUGIN_VERSION = "5.1.0.0";
     }
 }
