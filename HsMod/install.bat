5f9e00c464bddaf3479a875e68c8656e6be842aa
henryz <henryz@123.com>
2024-11-03 13:08:10 +0800

    修复

--- HsMod/install.bat
+++ HsMod/install.bat
@@ -1,5 +1,1 @@
-if exist "%PROGRAMFILES(x86)%\Hearthstone\BepInEx\core\BepInEx.dll" (
-    copy /y Release\HsMod.dll "D:\code\HSCentric\default_runtime\BepinEXPlugin\HsMod.dll"
-) else (
-    echo BepInEx is not exist!
-)
+copy /y HsMod\Release\HsMod.dll "D:\code\HSCentric\default_runtime\BepinEXPlugin\HsMod.dll"
